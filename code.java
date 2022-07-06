289589db34f0d105133e9ebae25a27a432d27324
“vijay” <vijay7245saravana@gmail.com>
2022-07-06 02:49:36 +0000

    Application1612

--- code.java
+++ code.java
@@ -1,7 +1,7 @@
 class Main {
   public static void main(String[] args) {
 
-    int num = 1234, reversed = 0;
+    int num = 12346789, reversed = 0;
     
     System.out.println("Original Number: " + num);
 
